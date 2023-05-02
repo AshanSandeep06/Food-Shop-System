17bddf8fcf48e222dd16f1e98ffebd4b5d68372a
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-02 18:45:41 +0530

    Content Updated with creating CORS middlware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,3 +5,16 @@ import express, {Request, Response} from 'express';
 import db from 'mongoose';
 import { json, urlencoded } from 'body-parser';
 import cors from 'cors';
+
+const app = express();
+
+const allowedOrigins = ["http://localhost:3000"];
+app.use(cors({
+    origin: (origin, callback) => {
+        if(allowedOrigins.indexOf(origin!) !== -1 || !origin){
+            callback(null, true);
+        }else {
+            callback(new Error("Not Allowed by CORS"));
+        }
+    }
+}));
