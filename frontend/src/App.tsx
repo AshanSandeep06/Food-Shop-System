64ed2c98abd9ea446ffc68db359c76a47920c129
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-02 23:20:26 +0530

    Content Updated

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,9 @@ import logo from "./logo.svg";
 import "./App.css";
 
 const App = () => {
-  return <div className="App"></div>;
+  return <div className="App">
+    <a href="#" id="scroll_up_btn"><i className="fa-solid fa-arrow-up"></i></a>
+  </div>;
 };
 
 export default App;
