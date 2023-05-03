75792776847be07c4442f68f568f3453c6b6bfc9
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-03 15:45:42 +0530

    Content Updated

--- frontend/src/components/Content/Content.tsx
+++ frontend/src/components/Content/Content.tsx
@@ -4,11 +4,6 @@ import { Route, Routes } from "react-router-dom";
 const Content = () => {
   return (
     <main className="mt-20">
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/" element={<Foods />}></Route>
-        <Route path="/" element={<Contact />}></Route>
-      </Routes>
     </main>
   );
 };
