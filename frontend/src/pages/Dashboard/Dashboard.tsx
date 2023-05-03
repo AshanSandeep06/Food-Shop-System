46763bd55a157e3d06b34f4f2403b0423bc9b03a
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-03 23:38:19 +0530

    Content Updated

--- frontend/src/pages/Dashboard/Dashboard.tsx
+++ frontend/src/pages/Dashboard/Dashboard.tsx
@@ -56,7 +56,7 @@ const Dashboard = () => {
   };
 
   return (
-    <div>
+    <>
       <div className="py-2 flex-1 flex flex-col items-start justify-center gap-3">
         <div className="flex items-center gap-2 justify-center bg-orange-100 px-4 py-1 rounded-full">
           <p className="text-base text-orange-500 font-bold">Bike Delivery</p>
@@ -99,7 +99,7 @@ const Dashboard = () => {
         />
         <StaticImages items={staticData.items} />
       </div>
-    </div>
+    </>
   );
 };
 
