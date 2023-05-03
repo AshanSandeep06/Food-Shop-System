fd5c5e10deed4b2cd9b51d3d8fb21f82b6d2559b
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-04 01:16:14 +0530

    Content Updated

--- frontend/src/pages/FoodMenu/FoodMenu.tsx
+++ frontend/src/pages/FoodMenu/FoodMenu.tsx
@@ -5,7 +5,7 @@ const FoodMenu = () => {
   return (
     <section className="w-full flex items-center flex-col bg-yellow-100 col-start-1 col-end-3 px-4 pb-8 pt-4">
       <div>
-        <h1 className="relative p-[8px]" id="foodMenuHeading">Our Food Menu</h1>
+        <h1 className="relative pb-[8px]" id="foodMenuHeading">Our Food Menu</h1>
       </div>
 
       <div>
