e0e504f102e16756a1459cdc9b568373eb698620
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-04 00:27:41 +0530

    Content Updated

--- frontend/src/components/Content/Content.tsx
+++ frontend/src/components/Content/Content.tsx
@@ -5,8 +5,8 @@ const Content = () => {
   return (
     <main className="mt-20 grid grid-cols-1 md:grid-cols-2 gap-2 w-full py-6 px-10 bg-[#F5F3F3]">
       <Dashboard />
-
       {/* ------------ Food Menu ------------ */}
+      
     </main>
   );
 };
