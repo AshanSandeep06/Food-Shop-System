4b486bd3688ff1dfe0654a15bcbae3cec17ce465
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-03 19:00:03 +0530

    Content Updated in Home Page

--- frontend/src/components/Content/Content.tsx
+++ frontend/src/components/Content/Content.tsx
@@ -1,10 +1,47 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import deliveryPhoto from "../../assets/img/delivery.png";
+import { motion } from "framer-motion";
 
 const Content = () => {
-  return <main className="mt-20">
-    
-  </main>;
+  return (
+    <main className="mt-20 grid grid-cols-1 md:grid-cols-2 gap-2 w-full py-6 px-10 bg-[#F5F3F3]">
+      <div className="py-2 flex-1 flex flex-col items-start justify-center gap-3">
+        <div className="flex items-center gap-2 justify-center bg-orange-100 px-4 py-1 rounded-full">
+          <p className="text-base text-orange-500 font-bold">Bike Delivery</p>
+          <div className="w-8 h-8 rounded-full overflow-hidden bg-white drop-shadow-xl">
+            <img
+              src={deliveryPhoto}
+              alt="delivery"
+              className="w-full h-full object-contain"
+            />
+          </div>
+        </div>
+        <p className="text-[2rem] lg:text-[4rem] font-bold tracking-wide text-headingColor">
+          The Fastest Food Delivery in
+          <span className="text-orange-600 text-[2.5rem] lg:text-[4.0rem]">
+            {" "}
+            Galle Fort
+          </span>
+        </p>
+        <p className="text-base text-textColor text-center md:text-left md:w-[80%]">
+          At our food shop, we are passionate about providing our customers with
+          high-quality and delicious food options. We take pride in sourcing
+          only the freshest and most wholesome ingredients, ensuring that every
+          dish we prepare is not only tasty but also healthy.
+        </p>
+        <motion.button
+          whileHover={{ scale: 1.1 }}
+          className="bg-gradient-to-br from-orange-400 to-orange-500 w-full 
+          md:w-auto px-4 py-2 rounded-lg hover:shadow-lg transition-all 
+          ease-in-out duration-100 !text-white"
+        >
+          Order Now
+        </motion.button>
+      </div>
+
+    </main>
+  );
 };
 
 export default Content;
