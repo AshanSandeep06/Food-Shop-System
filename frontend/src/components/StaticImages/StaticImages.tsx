31ebcab6d74e5f922f623d98803f81340b0fcd64
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-04 00:21:23 +0530

    Content Updated

--- frontend/src/components/StaticImages/StaticImages.tsx
+++ frontend/src/components/StaticImages/StaticImages.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { StaticFoodsList, StaticFoodItems } from "../../types/StaticFoodItems";
+import "../StaticImages/StaticImages.css"
 
 const StaticImages: React.FunctionComponent<StaticFoodsList> = ({ items }) => {
   return (
-    <div className="w-full h-full absolute flex items-center justify-center top-6 left-0 lg:px-30 lg:py-4 gap-5 md:gap-8 flex-wrap text-[#4a4343] lg:-translate-x-5">
+    <div id="foodsImagesContainer" className="w-full h-full absolute flex items-center justify-center top-6 left-0 lg:px-30 lg:py-4 gap-5 md:gap-8 flex-wrap text-[#4a4343] lg:-translate-x-5">
       {items.map((item, index) => (
         <div
           key={index}
