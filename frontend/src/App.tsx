2a3ce97793bbfb71da7a6ebad4827079100590ec
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-03 13:50:48 +0530

    Content Updated in Header component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,7 +14,6 @@ const App = () => {
 
       {/* =========== Header =========== */}
       <Header />
-      <Content />
     </div>
   );
 };
--- frontend/src/components/Content/Content.tsx
+++ frontend/src/components/Content/Content.tsx
@@ -2,38 +2,8 @@ import React from "react";
 
 const Content = () => {
   return (
-    <div className="bg-gray-50 h-[500px] bg-blue-700 relative top-20">
+    <div>
       <h1>Content</h1>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae
-        architecto vitae dolore ratione nemo incidunt officia nisi ducimus
-        assumenda iure, illum eum similique. Eveniet incidunt veritatis nisi sit
-        recusandae ducimus!
-      </p>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae
-        architecto vitae dolore ratione nemo incidunt officia nisi ducimus
-        assumenda iure, illum eum similique. Eveniet incidunt veritatis nisi sit
-        recusandae ducimus!
-      </p>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae
-        architecto vitae dolore ratione nemo incidunt officia nisi ducimus
-        assumenda iure, illum eum similique. Eveniet incidunt veritatis nisi sit
-        recusandae ducimus!
-      </p>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae
-        architecto vitae dolore ratione nemo incidunt officia nisi ducimus
-        assumenda iure, illum eum similique. Eveniet incidunt veritatis nisi sit
-        recusandae ducimus!
-      </p>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae
-        architecto vitae dolore ratione nemo incidunt officia nisi ducimus
-        assumenda iure, illum eum similique. Eveniet incidunt veritatis nisi sit
-        recusandae ducimus!
-      </p>
     </div>
   );
 };
