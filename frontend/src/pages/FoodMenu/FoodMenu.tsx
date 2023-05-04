5acdda83a39c084df3ab80858538716b332cdb00
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-04 10:18:36 +0530

    Content Updated

--- frontend/src/pages/FoodMenu/FoodMenu.tsx
+++ frontend/src/pages/FoodMenu/FoodMenu.tsx
@@ -47,7 +47,9 @@ const FoodMenu = () => {
         </div>
 
         <div className="border-2 border-black h-32 min-w-[8rem] rounded-lg flex flex-col items-center justify-center gap-5">
-          <span className="rounded-full border-2 border-white w-10 h-10 flex justify-center items-center"></span>
+          <span className="rounded-full border-2 border-white w-10 h-10 flex justify-center items-center">
+            <RestaurantMenuIcon />
+          </span>
           <span>Rice</span>
         </div>
 
