7a5fb8372be088ebe0bcf7f5e95053c075afb9f7
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-04 17:31:06 +0530

    Content Updated

--- frontend/src/components/Food/Food.tsx
+++ frontend/src/components/Food/Food.tsx
@@ -9,7 +9,7 @@ const Food = (props: StaticFoodsList) => {
       {props.items.map((item) => (
         <div
           key={item._id}
-          className="mt-10 cursor-pointer !h-[220px] !w-[275px] lg:min-w-[250px] drop-shadow-lg p-2 bg-[rgba(256,256,256,0.4)] backdrop-blur-md rounded-xl flex flex-col items-center justify-center"
+          className="mt-10 cursor-pointer !h-[265px] !w-[275px] lg:min-w-[250px] drop-shadow-lg p-2 bg-[rgba(256,256,256,0.4)] backdrop-blur-md rounded-xl flex flex-col items-center justify-center"
         >
           <motion.img
             whileHover={{ scale: 1.1 }}
@@ -24,15 +24,15 @@ const Food = (props: StaticFoodsList) => {
           <p className="text-[10px] lg:text-lg !text-[rgb(81,81,81)] !opacity-[0.7] font-semibold my-2 lg:my-3">
             {item.description}
           </p>
-          <p className="text-sm font-semibold text-headingColor">
+          <p className="text-sm font-semibold text-headingColor pb-[13px]">
             {item.price}{" "}
             <span className="text-xs text-red-600 relative bottom-[0.75px]">
               LKR
             </span>
           </p>
 
-          <span>
-            <AddShoppingCartIcon />
+          <span className="bg-[rgb(232,0,19)] rounded-full border-white w-10 h-10 flex justify-center items-center">
+            <AddShoppingCartIcon className="text-[#ece1e1]" />
           </span>
         </div>
       ))}
