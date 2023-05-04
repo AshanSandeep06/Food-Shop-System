344e6c9382bc885bf21b94c0f94d0a2748e17158
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-04 09:31:30 +0530

    Content Updated

--- frontend/src/pages/FoodMenu/FoodMenu.tsx
+++ frontend/src/pages/FoodMenu/FoodMenu.tsx
@@ -3,16 +3,14 @@ import "../FoodMenu/FoodMenu.css";
 
 const FoodMenu = () => {
   return (
-    <section
-      className="w-full flex items-center flex-col bg-yellow-100 px-4 pb-8 pt-4"
-    >
+    <section className="w-full flex items-center flex-col bg-yellow-100 px-4 pb-8 pt-4">
       <div className="mb-7">
         <h1 className="relative pb-[8px]" id="foodMenuHeading">
           Our Food Menu
         </h1>
       </div>
 
-      <div className="bg-blue-700 overflow-x-hidden flex w-full px-40 py-10 text-white justify-center items-center">
+      <div className="bg-blue-700 overflow-x-scroll scrollbar-hidden scroll-smooth flex w-full px-40 py-10 text-white justify-center items-center">
         <div className="border-2 border-black h-32 min-w-[8rem]">Menu</div>
         <div className="border-2 border-black h-32 min-w-[8rem]">Chicken</div>
         <div className="border-2 border-black h-32 min-w-[8rem]">Beverages</div>
