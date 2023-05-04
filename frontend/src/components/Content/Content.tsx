fdf11680676259abb359f90aa8ab2efaa855c230
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-04 09:01:19 +0530

    Content Updated in Contact component

--- frontend/src/components/Content/Content.tsx
+++ frontend/src/components/Content/Content.tsx
@@ -4,11 +4,16 @@ import FoodMenu from "../../pages/FoodMenu/FoodMenu";
 
 const Content = () => {
   return (
-    <main className="mt-20 grid grid-cols-1 md:grid-cols-2 gap-2 w-full py-6 px-10 bg-[#F5F3F3]">
-      <Dashboard />
-      {/* ------------ Food Menu ------------ */}
-      <FoodMenu />
-    </main>
+    <>
+      <main className="mt-20 grid grid-cols-1 md:grid-cols-2 gap-2 w-full py-6 px-10 bg-[#F5F3F3]">
+        <Dashboard />
+      </main>
+
+      <main>
+        {/* ------------ Food Menu ------------ */}
+        <FoodMenu />
+      </main>
+    </>
   );
 };
 
