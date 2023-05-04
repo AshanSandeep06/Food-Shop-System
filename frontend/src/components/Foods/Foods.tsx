9132e1a0354af0f6c0593dda173896c1477f03e2
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-04 12:18:38 +0530

    Content Updated

--- frontend/src/components/Foods/Foods.tsx
+++ frontend/src/components/Foods/Foods.tsx
@@ -4,11 +4,11 @@ import { StaticFoodsList } from "../../types/StaticFoodItems";
 
 const Foods = (props: StaticFoodsList) => {
   return (
-    <div className="w-full h-full flex items-center justify-center top-6 left-0 lg:px-30 lg:py-4 gap-5 md:gap-8 flex-wrap text-[#4a4343] xl:-translate-x-2">
+    <div className="w-full h-full flex items-center justify-center top-6 left-0 lg:px-30 lg:pr-4 gap-5 md:gap-8 flex-wrap text-[#4a4343] xl:-translate-x-2">
       {props.items.map((item, index) => (
         <div
           key={item._id}
-          className="mt-10 cursor-pointer !h-[210px] w-[200px] lg:min-w-[200px] drop-shadow-lg p-2 bg-[rgba(256,256,256,0.4)] backdrop-blur-md rounded-xl flex flex-col items-center justify-center"
+          className="mt-10 cursor-pointer !h-[210px] !w-[250px] lg:min-w-[200px] drop-shadow-lg p-2 bg-[rgba(256,256,256,0.4)] backdrop-blur-md rounded-xl flex flex-col items-center justify-center"
         >
           <motion.img
             whileHover={{ scale: 1.1 }}
@@ -17,7 +17,7 @@ const Foods = (props: StaticFoodsList) => {
             alt="foodImage"
             className="w-40 lg:w-40 -mt-10 lg:-mt-20 h-40 object-contain"
           />
-          <p className="text-base lg:text-lg font-semibold text-textColor">
+          <p className="text-md lg:text-lg font-semibold text-textColor">
             {item.title}
           </p>
           <p className="text-[10px] lg:text-lg !text-[rgb(81,81,81)] !opacity-[0.7] font-semibold my-2 lg:my-3">
