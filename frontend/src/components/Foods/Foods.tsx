bf34d38fb7dccfb955fed9ce90c748162a5c9f86
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-04 11:51:46 +0530

    Content Updated

--- frontend/src/components/Foods/Foods.tsx
+++ frontend/src/components/Foods/Foods.tsx
@@ -1,7 +1,41 @@
 import React from "react";
+import { StaticFoodsList } from "../../types/StaticFoodItems";
+import { motion } from "framer-motion";
 
-const Foods = () => {
-  return <div>Foods</div>;
-};
+const Foods = (props: StaticFoodsList) => {
+    return (
+      <div
+        id="foodsImagesContainer"
+        className="w-full h-full flex items-center justify-center top-6 left-0 lg:px-30 lg:py-4 gap-5 md:gap-8 flex-wrap text-[#4a4343] xl:-translate-x-2"
+      >
+        {props.items.map((item, index) => (
+          <div
+            key={item._id}
+            className="cursor-pointer !h-[210px] w-[150px] lg:min-w-[200px] drop-shadow-lg p-2 bg-[rgba(256,256,256,0.4)] backdrop-blur-md rounded-xl flex flex-col items-center justify-center"
+          >
+            <motion.img
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 1.1 }}
+              src={item.imagePath}
+              alt="foodImage"
+              className="w-24 lg:w-40 -mt-10 lg:-mt-20"
+            />
+            <p className="text-base lg:text-lg font-semibold text-textColor">
+              {item.title}
+            </p>
+            <p className="text-[10px] lg:text-lg !text-[rgb(81,81,81)] !opacity-[0.7] font-semibold my-2 lg:my-3">
+              {item.description}
+            </p>
+            <p className="text-sm font-semibold text-headingColor">
+              {item.price}{" "}
+              <span className="text-xs text-red-600 relative bottom-[0.75px]">
+                LKR
+              </span>
+            </p>
+          </div>
+        ))}
+      </div>
+    );
+  };
 
 export default Foods;
