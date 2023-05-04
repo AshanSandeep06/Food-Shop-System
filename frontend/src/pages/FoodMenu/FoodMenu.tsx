acf8d03acc98e8c6fd48f375db4b34f9ef97ef77
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-04 08:54:31 +0530

    Content Updated

--- frontend/src/pages/FoodMenu/FoodMenu.tsx
+++ frontend/src/pages/FoodMenu/FoodMenu.tsx
@@ -7,13 +7,13 @@ const FoodMenu = () => {
       className="w-full flex items-center flex-col bg-yellow-100
     col-start-1 col-end-3 px-4 pb-8 pt-4"
     >
-      <div>
+      <div className="mb-7">
         <h1 className="relative pb-[8px]" id="foodMenuHeading">
           Our Food Menu
         </h1>
       </div>
 
-      <div>
+      <div className="bg-blue-700">
         <div>Menu</div>
         <div>Chicken</div>
         <div>Beverages</div>
