ef62065a2a479cbc70b42a4b916e3f9bccbde1ac
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-05 00:36:16 +0530

    Crated CartItem Componet

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -27,6 +27,7 @@ import InboxIcon from "@mui/icons-material/MoveToInbox";
 import MailIcon from "@mui/icons-material/Mail";
 import { relative } from "path";
 import MyButton from "../MyButton/MyButton";
+import CartItem from "../CartItem/CartItem";
 
 type HeaderProps = {
   buttons: JSX.Element[];
@@ -88,36 +89,17 @@ const Header = (props: HeaderProps) => {
         <div className="flex h-screen flex-col bg-[#F5EEE9]">
           <div className="flex flex-col pt-3 pl-5 pr-5 gap-3 h-3/4 overflow-y-scroll scrollbar-hidden scroll-smooth">
             {/* cart items */}
-            <div className="pl-3 gap-2 flex items-center justify-start drop-shadow-lg bg-[rgba(256,256,256,0.4)] backdrop-blur-md rounded-xl">
-              <img
-                src={ChickenPlate}
-                alt="foodImage"
-                className="mr-[6px] w-20 h-20 max-w-[65px] rounded-full object-contain"
-              />
-              <div className="flex flex-col mr-[6px]">
-                <span>Chicken Plate</span>
-                <span>850
-                  <span className="text-[#ed1e2f] text-[13px]">  LKR</span>
-                </span>
-              </div>
-
-              <div className="flex gap-3 justify-center items-center">
-                <span>
-                  <button>-</button>
-                </span>
-                <span className="text-sm text-gray-50 w-5 h-5 rounded-sm bg-[#282a2c] flex items-center justify-center cursor-default">
-                  1
-                </span>
-                <span>
-                  <button>+</button>
-                </span>
-                <span>
-                  <button className="flex justify-center items-center w-6 h-6 rounded-lg text-gray-50 bg-[rgb(232,0,19)]">
-                    <DeleteIcon className="!text-[16px]" />
-                  </button>
-                </span>
-              </div>
-            </div>
+            <CartItem />
+            <CartItem />
+            <CartItem />
+            <CartItem />
+            <CartItem />
+            <CartItem />
+            <CartItem />
+            <CartItem />
+            <CartItem />
+            <CartItem />
+            <CartItem />
           </div>
 
           <div></div>
