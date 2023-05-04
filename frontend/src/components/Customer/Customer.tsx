d624cb1870780370feb7934c6023322d98fa4dfe
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-04 17:13:03 +0530

    Content Updated

--- frontend/src/components/Customer/Customer.tsx
+++ frontend/src/components/Customer/Customer.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import FoodMenu from "../../pages/FoodMenu/FoodMenu";
+import { Route, Routes } from "react-router-dom";
 
 const Customer = () => {
   return (
     <main className="mt-20 gap-2 w-full py-7 px-10 bg-[#F5F3F3]">
-      <FoodMenu />
+      <Routes>
+        <Route path="/home" element={<FoodMenu />}></Route>
+      </Routes>
     </main>
   );
 };
