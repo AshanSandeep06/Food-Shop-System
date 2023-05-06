32dc3526955d29a7c26b85a37b208e66a71afabe
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-06 14:33:19 +0530

    Content Updated in PlaceOrder Component

--- frontend/src/pages/PlaceOrder/PlaceOrder.tsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "../PlaceOrder/PlaceOrder.css";
 import Table from "../../components/Table/Table";
+import { TextField } from "@mui/material";
 
 const PlaceOrder = () => {
   return (
@@ -24,22 +25,15 @@ const PlaceOrder = () => {
       {/* --------- Form --------- */}
       <section className="bg-orange-300 py-3 px-20">
         <form className="h-full grid grid-cols-3">
-          <label className="block">
-            <span className="block text-sm font-medium text-slate-700">
-              Username
-            </span>
-            <input
-              type="text"
-              value="tbone"
-              disabled
-              className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
-      focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
-      disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
-      invalid:border-pink-500 invalid:text-pink-600
-      focus:invalid:border-pink-500 focus:invalid:ring-pink-500
-    "
-            />
-          </label>
+          <TextField
+            label="Category"
+            type="text"
+            variant="outlined"
+            name="categoryName"
+            placeholder="Enter Category Name"
+            fullWidth={true}
+            required
+          />
         </form>
       </section>
     </section>
