166f7be5442c9ce933685cd76a8a7b553e2146c9
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-06 17:27:13 +0530

    Content Updated

--- frontend/src/components/Table/Table.tsx
+++ frontend/src/components/Table/Table.tsx
@@ -22,10 +22,10 @@ const Table = (props: TableProps) => {
           {/* Your Items Cart */}
           {props.tblName}
         </h3>
-        <div className="w-full pb-6 rounded-2 overflow-x-auto scroll-smooth">
+        <div className="w-full pb-6 rounded-2">
           <table
             id="tblCartItems"
-            className="table-fixed border-collapse grid grid-cols-1"
+            className="table-fixed border-collapse grid grid-cols-1 overflow-x-auto scroll-smooth"
           >
             <thead
               className="text-white grid grid-cols-1 h-[43px]"
@@ -47,7 +47,7 @@ const Table = (props: TableProps) => {
               </tr>
             </thead>
 
-            <tbody className="cursor-pointer h-[185px]">
+            <tbody className="cursor-pointer h-[181px]">
               {props.tblData.map((dataRow, index) => (
                 <tr
                   key={index}
