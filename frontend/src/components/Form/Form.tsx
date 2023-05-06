1f0bd3be330281b0bff61435d19864e76ebded59
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-06 16:07:46 +0530

    Refactored Form Component

--- frontend/src/components/Form/Form.tsx
+++ frontend/src/components/Form/Form.tsx
@@ -1,8 +1,59 @@
+import { Button, TextField } from '@mui/material';
 import React from 'react'
+import SendIcon from "@mui/icons-material/Send";
 
 const Form = () => {
   return (
-    <div>Form</div>
+    <section className="py-10 px-6 lg:px-16 border-2 border-black">
+        <form className="grid grid-cols-1 lg:grid-cols-3 gap-6 border-2 border-yellow-400">
+          <TextField
+            label="Your Name"
+            type="text"
+            variant="outlined"
+            name="name"
+            placeholder="Your Name"
+            required
+          />
+
+          <TextField
+            label="Contact Number"
+            type="text"
+            variant="outlined"
+            name="contactNumber"
+            placeholder="Contact Number"
+            required
+          />
+
+          <TextField
+            label="Address"
+            type="text"
+            variant="outlined"
+            name="address"
+            placeholder="Address"
+            required
+          />
+
+          <div className="lg:col-end-4 lg:col-span-2 flex md:justify-end items-center justify-center flex-wrap gap-5">
+            <Button
+              className="!px-[10px] !capitalize !font-poppins !font-normal !text-[15px]"
+              variant="contained"
+              color="error"
+              endIcon={<SendIcon />}
+            >
+              Cancel Order
+            </Button>
+
+            <Button
+              className="!capitalize !font-poppins !font-normal !text-[15px]"
+              variant="contained"
+              color="success"
+              endIcon={<SendIcon />}
+            >
+              Place Order
+            </Button>
+          </div>
+        </form>
+      </section>
   )
 }
 
--- frontend/src/pages/PlaceOrder/PlaceOrder.tsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.tsx
@@ -3,6 +3,7 @@ import "../PlaceOrder/PlaceOrder.css";
 import Table from "../../components/Table/Table";
 import { Button, TextField } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
+import Form from "../../components/Form/Form";
 
 const PlaceOrder = () => {
   return (
@@ -24,56 +25,6 @@ const PlaceOrder = () => {
       />
 
       {/* --------- Form --------- */}
-      <section className="py-10 px-6 lg:px-16 border-2 border-black">
-        <form className="grid grid-cols-1 lg:grid-cols-3 gap-6 border-2 border-yellow-400">
-          <TextField
-            label="Your Name"
-            type="text"
-            variant="outlined"
-            name="name"
-            placeholder="Your Name"
-            required
-          />
-
-          <TextField
-            label="Contact Number"
-            type="text"
-            variant="outlined"
-            name="contactNumber"
-            placeholder="Contact Number"
-            required
-          />
-
-          <TextField
-            label="Address"
-            type="text"
-            variant="outlined"
-            name="address"
-            placeholder="Address"
-            required
-          />
-
-          <div className="lg:col-end-4 lg:col-span-2 flex md:justify-end items-center justify-center flex-wrap gap-5">
-            <Button
-              className="!px-[10px] !capitalize !font-poppins !font-normal !text-[15px]"
-              variant="contained"
-              color="error"
-              endIcon={<SendIcon />}
-            >
-              Cancel Order
-            </Button>
-
-            <Button
-              className="!capitalize !font-poppins !font-normal !text-[15px]"
-              variant="contained"
-              color="success"
-              endIcon={<SendIcon />}
-            >
-              Place Order
-            </Button>
-          </div>
-        </form>
-      </section>
     </section>
   );
 };
