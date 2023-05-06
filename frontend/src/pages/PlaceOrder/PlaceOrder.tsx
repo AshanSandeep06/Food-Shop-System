fa44f4e230af43ef2c65ae52afc2e4bcca83da97
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-06 14:23:46 +0530

    Content Updated

--- frontend/src/pages/PlaceOrder/PlaceOrder.tsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.tsx
@@ -22,8 +22,25 @@ const PlaceOrder = () => {
       />
 
       {/* --------- Form --------- */}
-      <section className="bg-orange-300">
-        <h1>Form</h1>
+      <section className="bg-orange-300 py-3 px-20">
+        <form className="h-full grid grid-cols-3">
+          <label className="block">
+            <span className="block text-sm font-medium text-slate-700">
+              Username
+            </span>
+            <input
+              type="text"
+              value="tbone"
+              disabled
+              className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+      focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+      disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+      invalid:border-pink-500 invalid:text-pink-600
+      focus:invalid:border-pink-500 focus:invalid:ring-pink-500
+    "
+            />
+          </label>
+        </form>
       </section>
     </section>
   );
