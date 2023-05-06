9b39bc334bd662a34ddfd3039e466b0f9c8a4a36
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-06 12:07:48 +0530

    Content Updated in PlaceOrder Component

--- frontend/src/pages/PlaceOrder/PlaceOrder.tsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.tsx
@@ -8,8 +8,17 @@ const PlaceOrder = () => {
       {/* ----- Cart Items Table ----- */}
       <Table
         tblName="Your Items Cart"
-        tblHeaders={["Item Code", "Description", "Item Image", "Unit Price", "Quantity"]}
-        tblData={[["I001", "Soap", "Uploaded", "350.00", "10"], ["I002", "Rice"]]}
+        tblHeaders={[
+          "Item Code",
+          "Description",
+          "Item Image",
+          "Unit Price",
+          "Quantity",
+        ]}
+        tblData={[
+          ["I001", "Soap", "Uploaded", "350.00", "10"],
+          ["I002", "Rice"],
+        ]}
       />
 
       {/* --------- Form --------- */}
