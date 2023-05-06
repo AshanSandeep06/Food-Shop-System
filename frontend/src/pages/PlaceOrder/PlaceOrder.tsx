6bb57055e2a123063b0bd5eebad87b003606454e
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-06 16:48:31 +0530

    Content Updated

--- frontend/src/pages/PlaceOrder/PlaceOrder.tsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.tsx
@@ -4,7 +4,7 @@ import Table from "../../components/Table/Table";
 import { Button, Paper, TextField } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
 import Form from "../../components/Form/Form";
-import Divider from '@mui/material/Divider';
+import Divider from "@mui/material/Divider";
 
 const PlaceOrder = () => {
   return (
@@ -29,22 +29,46 @@ const PlaceOrder = () => {
 
       {/* --------- Payment --------- */}
       <section className="my-8">
-      <Paper elevation={3}  className="w-max border-2 border-black flex flex-col gap-4 p-4">
-        <div className="flex justify-center items-center gap-3">
-        <span className="text-base md:text-lg ">Sub Total</span>
-        <span>-</span>
-        <span>2500.00 <span className="text-sm text-red-600 relative bottom-[0.75px]">LKR</span></span>
-        </div>
+        <Paper
+          elevation={3}
+          className="w-max border-2 border-black flex flex-col gap-4 p-4"
+        >
+          <div className="flex justify-center items-center gap-3">
+            <span className="text-base md:text-lg ">Sub Total</span>
+            <span>-</span>
+            <span>
+              2500.00{" "}
+              <span className="text-sm text-red-600 relative bottom-[0.75px]">
+                LKR
+              </span>
+            </span>
+          </div>
 
-        <div className="flex justify-center items-center gap-3">
-        <span className="text-base md:text-lg ">Sub Total</span>
-        <span>-</span>
-        <span>2500.00 <span className="text-sm text-red-600 relative bottom-[0.75px]">LKR</span></span>
-        </div>
-        
-        <Divider />
-      </Paper>
-     </section>
+          <div className="flex justify-center items-center gap-3">
+            <span className="text-base md:text-lg ">Sub Total</span>
+            <span>-</span>
+            <span>
+              2500.00{" "}
+              <span className="text-sm text-red-600 relative bottom-[0.75px]">
+                LKR
+              </span>
+            </span>
+          </div>
+
+          <Divider />
+
+          <div className="flex justify-center items-center gap-3">
+            <span className="text-base md:text-lg ">Sub Total</span>
+            <span>-</span>
+            <span>
+              2500.00{" "}
+              <span className="text-sm text-red-600 relative bottom-[0.75px]">
+                LKR
+              </span>
+            </span>
+          </div>
+        </Paper>
+      </section>
 
       {/* ===================================================== */}
 
