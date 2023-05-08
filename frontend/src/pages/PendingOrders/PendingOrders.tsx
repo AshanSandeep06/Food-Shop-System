54ac204dbfcd53f7925b164e54ae4490d9553e49
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-08 19:50:13 +0530

    Content Updated in PendingOrders Component

--- frontend/src/pages/PendingOrders/PendingOrders.tsx
+++ frontend/src/pages/PendingOrders/PendingOrders.tsx
@@ -19,9 +19,39 @@ const PendingOrders = () => {
           "Order Status",
         ]}
         tblData={[
-          ["OID-001", "C00-001", "Sunil Bandara", "0774592741", "1500.00", "250.00", "1750.00", "46/D, Makuluwa, Galle", "Pending"],
-          ["OID-001", "C00-001", "Sunil Bandara", "0774592741", "1500.00", "250.00", "1750.00", "46/D, Makuluwa, Galle", "Pending"],
-          ["OID-001", "C00-001", "Sunil Bandara", "0774592741", "1500.00", "250.00", "1750.00", "46/D, Makuluwa, Galle", "Pending"],
+          [
+            "OID-001",
+            "C00-001",
+            "Sunil Bandara",
+            "0774592741",
+            "1500.00",
+            "250.00",
+            "1750.00",
+            "46/D, Makuluwa, Galle",
+            "Pending",
+          ],
+          [
+            "OID-001",
+            "C00-001",
+            "Sunil Bandara",
+            "0774592741",
+            "1500.00",
+            "250.00",
+            "1750.00",
+            "46/D, Makuluwa, Galle",
+            "Pending",
+          ],
+          [
+            "OID-001",
+            "C00-001",
+            "Sunil Bandara",
+            "0774592741",
+            "1500.00",
+            "250.00",
+            "1750.00",
+            "46/D, Makuluwa, Galle",
+            "Pending",
+          ],
         ]}
       />
     </section>
