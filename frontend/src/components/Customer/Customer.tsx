cff5caf552435b3dd0dabf3b8d1caccc1c99e196
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-08 18:43:45 +0530

    Content Updated in Header Component

--- frontend/src/components/Customer/Customer.tsx
+++ frontend/src/components/Customer/Customer.tsx
@@ -23,7 +23,7 @@ const Customer = () => {
       <Routes>
         <Route path="/home" element={<FoodMenu />}></Route>
         <Route path="/placeOrder" element={<PlaceOrder />}></Route>
-        <Route path="/pending_Orders" element={<PendingOrders />}></Route>
+        <Route path="/pending_orders" element={<PendingOrders />}></Route>
       </Routes>
     </main>
   );
