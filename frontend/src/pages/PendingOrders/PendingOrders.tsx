71a872e77287dd092045784187852bd0e6e92d6d
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-08 19:03:20 +0530

    Content Updated in PendingOrders Component

--- frontend/src/pages/PendingOrders/PendingOrders.tsx
+++ frontend/src/pages/PendingOrders/PendingOrders.tsx
@@ -1,9 +1,22 @@
 import React from "react";
+import Table from "../../components/Table";
 
 const PendingOrders = () => {
-  return <section>
-    
-  </section>;
+  return (
+    <section>
+      <Table
+        tblName="Your Items Cart"
+        tblHeaders={[
+          "Item Code",
+          "Description",
+          "Item Image",
+          "Unit Price",
+          "Quantity",
+        ]}
+        tblData={[["I001", "Soap", "Uploaded", "350.00", "10"]]}
+      />
+    </section>
+  );
 };
 
 export default PendingOrders;
