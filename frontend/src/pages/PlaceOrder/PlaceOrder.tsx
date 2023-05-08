a611c0fdf6f764f299f96a1018d5966770c36cc6
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-08 17:04:32 +0530

    Content Updated

--- frontend/src/pages/PlaceOrder/PlaceOrder.tsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import "../PlaceOrder/PlaceOrder.css";
 import Table from "../../components/Table/Table";
 import { Button, Paper, TextField } from "@mui/material";
@@ -6,7 +6,13 @@ import SendIcon from "@mui/icons-material/Send";
 import Form from "../../components/Form/Form";
 import Divider from "@mui/material/Divider";
 
+type PlaceOrderState = {
+  windowSize: number;
+};
+
 const PlaceOrder = () => {
+  const windowSize = useRef([window.innerWidth, window.innerHeight]);
+
   return (
     <section>
       {/* ----- Cart Items Table ----- */}
@@ -26,10 +32,13 @@ const PlaceOrder = () => {
 
       {/* --------- Payment --------- */}
       <section className="my-12 flex justify-center">
-        <Paper className="md:w-1/3 sm:w-3/4 w-full border border-slate-300 grid grid-cols-1 sm:flex sm:flex-col sm:gap-4 p-6 !bg-[#FAFAFF]" elevation={5}>
-          <div className="sm:flex sm:items-center sm:gap-6 !text-[rgb(81,81,81)]">
+        <Paper
+          className="md:w-1/3 sm:w-3/4 w-full border border-slate-300 grid grid-cols-1 sm:flex sm:flex-col sm:gap-4 p-6 !bg-[#FAFAFF]"
+          elevation={5}
+        >
+          <div className="grid sm:flex sm:items-center sm:gap-6 !text-[rgb(81,81,81)]">
             <span className="text-[18px] md:text-lg">Sub Total</span>
-            <span>-</span>
+            {windowSize.current[0] >= 640 ? <span>-</span> : null}
             <span className="text-[18px] md:text-lg">
               2500.00{" "}
               <span className="text-sm text-red-600 relative bottom-[0.75px]">
