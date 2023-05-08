a2db04f7bcc89fb0d917315d47c857ac66f66f44
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-08 18:23:44 +0530

    Content Updated in Customer Component

--- frontend/src/components/Customer/Customer.tsx
+++ frontend/src/components/Customer/Customer.tsx
@@ -15,6 +15,7 @@ import {
 import InboxIcon from "@mui/icons-material/MoveToInbox";
 import MailIcon from "@mui/icons-material/Mail";
 import PlaceOrder from "../../pages/PlaceOrder/PlaceOrder";
+import PendingOrders from "../../pages/PendingOrders";
 
 const Customer = () => {
   return (
