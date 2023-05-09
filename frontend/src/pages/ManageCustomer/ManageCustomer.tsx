345513914ce4b02f0034da34841a04813bb5573e
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-09 16:11:33 +0530

    Content Updated in ManageCustomer Component

--- frontend/src/pages/ManageCustomer/ManageCustomer.tsx
+++ frontend/src/pages/ManageCustomer/ManageCustomer.tsx
@@ -3,11 +3,14 @@ import React from "react";
 const ManageCustomer = () => {
   return (
     <section>
+        {/* 1 */}
       <section>
         <div>
           <h1>Manage Customer</h1>
         </div>
       </section>
+
+      {/* 2 */}
     </section>
   );
 };
