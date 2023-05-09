39312fa944f70133b74212863901b351e5a09435
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-09 16:03:50 +0530

    Initialized ManageCustomer Component

--- frontend/src/components/Admin/Admin.tsx
+++ frontend/src/components/Admin/Admin.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import FoodMenu from "../../pages/FoodMenu";
+import ManageCustomer from "../../pages/ManageCustomer";
 
 const Admin = () => {
   return (
     <main className="mt-20 gap-2 w-full pt-6 pb-9 px-10 bg-[#FAFAFF]">
       <Routes>
         <Route path="/home" element={<FoodMenu />}></Route>
-        <Route path="/manage_customer" element={<FoodMenu />}></Route>
+        <Route path="/manage_customer" element={<ManageCustomer />}></Route>
       </Routes>
     </main>
   );
---
+++ frontend/src/pages/ManageCustomer/ManageCustomer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ManageCustomer = () => {
+  return (
+    <div>ManageCustomer</div>
+  )
+}
+
+export default ManageCustomer;
