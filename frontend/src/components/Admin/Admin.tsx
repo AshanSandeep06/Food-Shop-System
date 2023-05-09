ae124e1223954a0c8ec9568cea0c5bdd976ead42
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-09 15:35:46 +0530

    Content Updated in Admin Component

--- frontend/src/components/Admin/Admin.tsx
+++ frontend/src/components/Admin/Admin.tsx
@@ -3,11 +3,13 @@ import { Route, Routes } from "react-router-dom";
 import FoodMenu from "../../pages/FoodMenu";
 
 const Admin = () => {
-  return <main className="mt-20 gap-2 w-full pt-6 pb-9 px-10 bg-[#FAFAFF]">
-  <Routes>
-    <Route path="/home" element={<FoodMenu />}></Route>
-  </Routes>
-</main>
+  return (
+    <main className="mt-20 gap-2 w-full pt-6 pb-9 px-10 bg-[#FAFAFF]">
+      <Routes>
+        <Route path="/home" element={<FoodMenu />}></Route>
+      </Routes>
+    </main>
+  );
 };
 
 export default Admin;
