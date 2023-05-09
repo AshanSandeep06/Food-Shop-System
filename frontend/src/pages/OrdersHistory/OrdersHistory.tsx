7cbfc527b0f9cc5e6d50449e43ed0881c7d9e08c
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-09 12:13:44 +0530

    Content Refactored in OrdersHistory Component

--- frontend/src/pages/OrdersHistory/OrdersHistory.tsx
+++ frontend/src/pages/OrdersHistory/OrdersHistory.tsx
@@ -1,9 +1,149 @@
-import React from 'react'
+import React from "react";
+import Table from "../../components/Table";
 
 const OrdersHistory = () => {
   return (
-    <div>OrdersHistory</div>
-  )
-}
+    <section>
+      <Table
+        tblName="Your Pending Orders"
+        tblHeight="auto"
+        tblHeaders={[
+          "Order ID",
+          "Customer ID",
+          "Name",
+          "Contact Number",
+          "Sub Total",
+          "Delivery Fee",
+          "Total Charge",
+          "Order Location",
+          "Order Status",
+          "Option",
+        ]}
+        tblData={[
+          [
+            "OID-001",
+            "C00-001",
+            "Sunil Bandara",
+            "0774592741",
+            "1500.00",
+            "250.00",
+            "1750.00",
+            "46/D, Makuluwa, Galle",
+            "Pending",
+            "Cancel",
+          ],
+          [
+            "OID-001",
+            "C00-001",
+            "Sunil Bandara",
+            "0774592741",
+            "1500.00",
+            "250.00",
+            "1750.00",
+            "46/D, Makuluwa, Galle",
+            "Pending",
+            "Cancel",
+          ],
+          [
+            "OID-001",
+            "C00-001",
+            "Sunil Bandara",
+            "0774592741",
+            "1500.00",
+            "250.00",
+            "1750.00",
+            "46/D, Makuluwa, Galle",
+            "Pending",
+            "Cancel",
+          ],
+          [
+            "OID-001",
+            "C00-001",
+            "Sunil Bandara",
+            "0774592741",
+            "1500.00",
+            "250.00",
+            "1750.00",
+            "46/D, Makuluwa, Galle",
+            "Pending",
+            "Cancel",
+          ],
+          [
+            "OID-001",
+            "C00-001",
+            "Sunil Bandara",
+            "0774592741",
+            "1500.00",
+            "250.00",
+            "1750.00",
+            "46/D, Makuluwa, Galle",
+            "Pending",
+            "Cancel",
+          ],
+          [
+            "OID-001",
+            "C00-001",
+            "Sunil Bandara",
+            "0774592741",
+            "1500.00",
+            "250.00",
+            "1750.00",
+            "46/D, Makuluwa, Galle",
+            "Pending",
+            "Cancel",
+          ],
+          [
+            "OID-001",
+            "C00-001",
+            "Sunil Bandara",
+            "0774592741",
+            "1500.00",
+            "250.00",
+            "1750.00",
+            "46/D, Makuluwa, Galle",
+            "Pending",
+            "Cancel",
+          ],
+          [
+            "OID-001",
+            "C00-001",
+            "Sunil Bandara",
+            "0774592741",
+            "1500.00",
+            "250.00",
+            "1750.00",
+            "46/D, Makuluwa, Galle",
+            "Pending",
+            "Cancel",
+          ],
+          [
+            "OID-001",
+            "C00-001",
+            "Sunil Bandara",
+            "0774592741",
+            "1500.00",
+            "250.00",
+            "1750.00",
+            "46/D, Makuluwa, Galle",
+            "Pending",
+            "Cancel",
+          ],
+          [
+            "OID-001",
+            "C00-001",
+            "Sunil Bandara",
+            "0774592741",
+            "1500.00",
+            "250.00",
+            "1750.00",
+            "46/D, Makuluwa, Galle",
+            "Pending",
+            "Cancel",
+          ],
+        ]}
+      />
+    </section>
+  );
+};
 
 export default OrdersHistory;
