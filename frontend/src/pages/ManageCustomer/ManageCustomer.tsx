f390661f23d2a5a5bb26597175960f7c4f3030be
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-09 16:19:50 +0530

    Content Updated in ManageCustomer Component

--- frontend/src/pages/ManageCustomer/ManageCustomer.tsx
+++ frontend/src/pages/ManageCustomer/ManageCustomer.tsx
@@ -1,6 +1,40 @@
+import { FormControl, InputBase, InputLabel, styled } from "@mui/material";
 import React from "react";
 
 const ManageCustomer = () => {
+  const BootstrapInput = styled(InputBase)(({ theme }) => ({
+    "label + &": {
+      marginTop: theme.spacing(3),
+    },
+    "& .MuiInputBase-input": {
+      borderRadius: 4,
+      position: "relative",
+      backgroundColor: theme.palette.background.paper,
+      border: "1px solid #ced4da",
+      fontSize: 16,
+      padding: "10px 26px 10px 12px",
+      transition: theme.transitions.create(["border-color", "box-shadow"]),
+      // Use the system font instead of the default Roboto font.
+      fontFamily: [
+        "-apple-system",
+        "BlinkMacSystemFont",
+        '"Segoe UI"',
+        "Roboto",
+        '"Helvetica Neue"',
+        "Arial",
+        "sans-serif",
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+      ].join(","),
+      "&:focus": {
+        borderRadius: 4,
+        borderColor: "#80bdff",
+        boxShadow: "0 0 0 0.2rem rgba(0,123,255,.25)",
+      },
+    },
+  }));
+
   return (
     <section>
       {/* 1 */}
@@ -12,10 +46,15 @@ const ManageCustomer = () => {
 
       {/* 2 */}
       <section>
-        <div></div>
+        <div>
+          <FormControl sx={{ m: 1 }} variant="standard">
+            <InputLabel htmlFor="demo-customized-textbox">Age</InputLabel>
+            <BootstrapInput id="demo-customized-textbox" />
+          </FormControl>
+        </div>
 
         <div></div>
-        
+
         <div></div>
       </section>
     </section>
