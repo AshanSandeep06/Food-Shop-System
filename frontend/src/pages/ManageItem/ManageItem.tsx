b4453c635cd310b8d89d5070f8f1a33252d1e70d
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-09 20:47:05 +0530

    Content Updated

--- frontend/src/pages/ManageItem/ManageItem.tsx
+++ frontend/src/pages/ManageItem/ManageItem.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const ManageItem = () => {
-  return (
-    <div>ManageItem</div>
-  )
-}
+  return <div>ManageItem</div>;
+};
 
 export default ManageItem;
