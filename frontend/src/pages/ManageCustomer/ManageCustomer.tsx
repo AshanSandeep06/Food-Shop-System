18706557d395819c8c2b36a3a4f7abce51641cd8
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-09 19:32:10 +0530

    Content Updated in ManageCustomer Component

--- frontend/src/pages/ManageCustomer/ManageCustomer.tsx
+++ frontend/src/pages/ManageCustomer/ManageCustomer.tsx
@@ -57,7 +57,7 @@ const ManageCustomer = () => {
         </div>
       </section>
 
-      <section className="my-6 gap-10 px-6 lg:px-28 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 justify-center flex-wrap">
+      <section className="mt-4 mb-7 gap-10 px-6 lg:px-28 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 justify-center flex-wrap">
         <div className="flex justify-center sm:justify-end items-center">
           <FormControl className="sm:w-full w-full md:w-full lg:w-3/4">
             <InputLabel id="demo-simple-select-label">Age</InputLabel>
