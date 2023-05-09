cfa938ff386c21506389489859822d732e00b61d
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-09 16:10:54 +0530

    Content Updated in ManageCustomer Component

--- frontend/src/pages/ManageCustomer/ManageCustomer.tsx
+++ frontend/src/pages/ManageCustomer/ManageCustomer.tsx
@@ -3,7 +3,11 @@ import React from "react";
 const ManageCustomer = () => {
   return (
   <section>
-
+    <section>
+        <div>
+            <h1>Manage Customer</h1>
+        </div>
+    </section>
   </section>
   );
 };
