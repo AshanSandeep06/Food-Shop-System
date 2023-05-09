91e865650e86a2695eecaaa4d3736b94a8a71564
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-09 15:35:16 +0530

    Content Refactored in Admin component

--- frontend/src/components/Admin/Admin.tsx
+++ frontend/src/components/Admin/Admin.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import FoodMenu from "../../pages/FoodMenu";
 
 const Admin = () => {
-  return <div>Admin</div>;
+  return <main className="mt-20 gap-2 w-full pt-6 pb-9 px-10 bg-[#FAFAFF]">
+  <Routes>
+    <Route path="/home" element={<FoodMenu />}></Route>
+  </Routes>
+</main>
 };
 
 export default Admin;
