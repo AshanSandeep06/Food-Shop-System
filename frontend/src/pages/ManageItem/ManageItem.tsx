60ef264736771e8996844c2e4edbccdbd03f5565
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-10 09:22:14 +0530

    Content Updated in ManageItem Component

--- frontend/src/pages/ManageItem/ManageItem.tsx
+++ frontend/src/pages/ManageItem/ManageItem.tsx
@@ -57,7 +57,7 @@ const ManageItem = () => {
       <section>
         <div className="flex items-center">
           <h1 className="font-medium text-[22px]">Manage Item</h1>
-          <FastfoodIcon className="ml-[5px]" />
+          <FastfoodIcon className="ml-[6px] relative bottom-[2.5px]" />
         </div>
       </section>
 
