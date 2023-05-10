e11e04b97cd4e07cd2e44884a5db4a749f08f5ba
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-10 19:59:53 +0530

    Content Updated in ManageItem Component

--- frontend/src/pages/ManageItem/ManageItem.tsx
+++ frontend/src/pages/ManageItem/ManageItem.tsx
@@ -146,8 +146,8 @@ const ManageItem = () => {
 
       <section>
         <div className="border-[6px] border-blue-700">
-          <label className="form-label d-block">Upload Your NIC Image</label>
-          <img id="customerNicImage" className="img-fluid" />
+          <label>Upload Item Image</label>
+          <img id="itemImage" className="img-fluid" />
           <input
             className="form-control mt-3 w-75"
             type="file"
