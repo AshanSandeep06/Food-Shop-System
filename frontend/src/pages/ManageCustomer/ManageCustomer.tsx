a89e2886df278b7a2b2632cf71a80095d9f688b8
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-10 09:03:54 +0530

    Content Updated in ManageCustomer Component

--- frontend/src/pages/ManageCustomer/ManageCustomer.tsx
+++ frontend/src/pages/ManageCustomer/ManageCustomer.tsx
@@ -72,7 +72,6 @@ const ManageCustomer = () => {
             >
               <MenuItem value={10}>Ten</MenuItem>
               <MenuItem value={20}>Twenty</MenuItem>
-              <MenuItem value={30}>Thirty</MenuItem>
             </Select>
           </FormControl>
         </div>
