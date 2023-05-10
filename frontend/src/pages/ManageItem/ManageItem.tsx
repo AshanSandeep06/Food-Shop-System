522e1ebf25308de7794776670eb8febedbb3bb09
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-10 19:04:49 +0530

    Content Updated in ManageItem Component

--- frontend/src/pages/ManageItem/ManageItem.tsx
+++ frontend/src/pages/ManageItem/ManageItem.tsx
@@ -155,6 +155,7 @@ const ManageItem = () => {
           tblHeaders={[
             "Item Code",
             "Name",
+            "View",
             "Description",
             "Unit Price",
             "QtyOnHand",
