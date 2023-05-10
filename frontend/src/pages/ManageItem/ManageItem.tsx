4c03bf3aa21a4bf00c83eb4ae14e1b7111861cc2
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-10 20:15:10 +0530

    Content Updated in ManageItem Component

--- frontend/src/pages/ManageItem/ManageItem.tsx
+++ frontend/src/pages/ManageItem/ManageItem.tsx
@@ -145,7 +145,7 @@ const ManageItem = () => {
         />
       </section>
 
-      <section className="flex justify-center border-[6px] border-blue-700">
+      <section className="mb-5 flex justify-center border-[6px] border-blue-700">
         <div className="bg-yellow-400 flex flex-col">
           <label className="mb-3">Upload Item Image</label>
           <img id="itemImage" />
