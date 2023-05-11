8b188d74c87b2ca033b69b0257cbe19eebc4bf93
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-11 11:01:33 +0530

    Content Updated in PlaceOrderForm Component

--- frontend/src/pages/Admin/PlaceOrderForm/PlaceOrderForm.tsx
+++ frontend/src/pages/Admin/PlaceOrderForm/PlaceOrderForm.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const PlaceOrderForm = () => {
-  return (
-    <div>PlaceOrderForm</div>
-  )
-}
+  return <div>PlaceOrderForm</div>;
+};
 
 export default PlaceOrderForm;
