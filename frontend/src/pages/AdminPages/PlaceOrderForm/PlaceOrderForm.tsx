55850acfdc0b98c716037e980ecd7c75751e2f89
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-11 13:00:54 +0530

    Content Updated

--- frontend/src/pages/AdminPages/PlaceOrderForm/PlaceOrderForm.tsx
+++ frontend/src/pages/AdminPages/PlaceOrderForm/PlaceOrderForm.tsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Form from "../../../components/Form/Form";
 import BackspaceIcon from "@mui/icons-material/Backspace";
 import { TextField } from "@mui/material";
 
 const PlaceOrderForm = () => {
+  const [currentDate, setcurrentDate] = useState<string>(
+    new Date().toISOString().split("T")[0]
+  );
+
+  useEffect(() => {
+    setTimeout(() => {
+      setcurrentDate();
+    }, 1000);
+  }, []);
+
+  const handleDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    // setSelectedDate(event.target.value);
+  };
+
   return (
     <section>
       <section>
@@ -22,6 +36,22 @@ const PlaceOrderForm = () => {
             defaultValue="OID-001"
             InputProps={{
               readOnly: true,
+            }}
+          />
+
+          <TextField
+            label="Order Date"
+            type="date"
+            variant="outlined"
+            name="orderDate"
+            placeholder="Order Date"
+            required
+            value={currentDate}
+            InputProps={{
+              readOnly: true,
+            }}
+            InputLabelProps={{
+              shrink: true,
             }}
           />
         </div>
