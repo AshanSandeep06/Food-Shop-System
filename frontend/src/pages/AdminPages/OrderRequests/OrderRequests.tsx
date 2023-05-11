173149d1b50ff06c089efa78e27ee7a6afebb41d
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-11 23:38:19 +0530

    Content Refactored in orderRequests Component

--- frontend/src/pages/AdminPages/OrderRequests/OrderRequests.tsx
+++ frontend/src/pages/AdminPages/OrderRequests/OrderRequests.tsx
@@ -1,8 +1,77 @@
+import { Chip } from '@mui/material';
 import React from 'react'
+import Table from '../../../components/Table';
 
 const OrderRequests = () => {
   return (
-    <div>OrderRequests</div>
+    <section>
+      <Table
+        tblName="My Pending Orders"
+        tblHeight="auto"
+        tblHeaders={[
+          "Order ID",
+          "Customer ID",
+          "Name",
+          "Contact Number",
+          "Sub Total",
+          "Delivery Fee",
+          "Total Charge",
+          "Order Location",
+          "Order Status",
+          "Option",
+        ]}
+        tblData={[
+          [
+            "OID-001",
+            "C00-001",
+            "Sunil Bandara",
+            "0774592741",
+            "1500.00",
+            "250.00",
+            "1750.00",
+            "46/D, Makuluwa, Galle",
+            <Chip
+              label="Pending"
+              color="primary"
+              style={{ fontFamily: "Poppins" }}
+            />,
+            "Cancel",
+          ],
+          [
+            "OID-001",
+            "C00-001",
+            "Sunil Bandara",
+            "0774592741",
+            "1500.00",
+            "250.00",
+            "1750.00",
+            "46/D, Makuluwa, Galle",
+            <Chip
+              label="Pending"
+              color="primary"
+              style={{ fontFamily: "Poppins" }}
+            />,
+            "Cancel",
+          ],
+          [
+            "OID-001",
+            "C00-001",
+            "Sunil Bandara",
+            "0774592741",
+            "1500.00",
+            "250.00",
+            "1750.00",
+            "46/D, Makuluwa, Galle",
+            <Chip
+              label="Pending"
+              color="primary"
+              style={{ fontFamily: "Poppins" }}
+            />,
+            "Cancel",
+          ],
+        ]}
+      />
+    </section>
   )
 }
 
