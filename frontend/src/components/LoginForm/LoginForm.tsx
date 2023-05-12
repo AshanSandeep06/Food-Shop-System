5e23048e046752dc2b0117446bbf2b4131b3c7f4
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-12 12:02:20 +0530

    Content Updated in LoginForm component

--- frontend/src/components/LoginForm/LoginForm.tsx
+++ frontend/src/components/LoginForm/LoginForm.tsx
@@ -7,7 +7,9 @@ const LoginForm = () => {
   return (
     <aside className="p-4">
       <div className="grid grid-cols-12 border-2 border-red-600 justify-center items-center p-2">
-        <h1 className="pl-10 col-span-10 text-center text-[25px] font-medium flex justify-center">Login Form</h1>
+        <h1 className="pl-10 col-span-10 text-center text-[25px] font-medium flex justify-center">
+          Login Form
+        </h1>
         <span className="p-2 col-span-2">
           <CloseIcon
             id="btnClose"
