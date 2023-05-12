cb43acd72cf134ca9550f106f2995f3afe3d4890
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-12 11:15:36 +0530

    Content Updated in LoginForm component

--- frontend/src/components/LoginForm/LoginForm.tsx
+++ frontend/src/components/LoginForm/LoginForm.tsx
@@ -1,16 +1,39 @@
+import { TextField } from "@mui/material";
 import React from "react";
 
 const LoginForm = () => {
   return (
     <aside>
+      <div>
+        <h1>Login Form</h1>
+        <span>Icon</span>
+      </div>
+
+      <div>
         <div>
-            <h1>Login Form</h1>
-            <span>Icon</span>
+          <TextField
+            label="Username"
+            type="text"
+            variant="outlined"
+            name="username"
+            placeholder="Username"
+            required
+          />
         </div>
 
         <div>
-            
+          <TextField
+            label="Password"
+            type="text"
+            variant="outlined"
+            name="password"
+            placeholder="Password"
+            required
+          />
         </div>
+      </div>
+
+      <div></div>
     </aside>
   );
 };
