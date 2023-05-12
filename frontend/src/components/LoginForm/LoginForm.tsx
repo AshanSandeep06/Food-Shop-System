a5a7679d73c629c5a25bff7b78dcaa51ef9a192d
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-12 11:13:08 +0530

    Content Updated in LoginForm component

--- frontend/src/components/LoginForm/LoginForm.tsx
+++ frontend/src/components/LoginForm/LoginForm.tsx
@@ -3,7 +3,14 @@ import React from "react";
 const LoginForm = () => {
   return (
     <aside>
-        
+        <div>
+            <h1>Login Form</h1>
+            <span>Icon</span>
+        </div>
+
+        <div>
+            
+        </div>
     </aside>
   );
 };
