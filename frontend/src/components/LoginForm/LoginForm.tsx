105d24dd8260c42c24bc1be36524a3be3ffaa644
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-12 11:18:31 +0530

    Content Updated in LoginForm component

--- frontend/src/components/LoginForm/LoginForm.tsx
+++ frontend/src/components/LoginForm/LoginForm.tsx
@@ -33,6 +33,40 @@ const LoginForm = () => {
         </div>
       </div>
 
+      <div>
+        <div
+          id="rdbRememberMe"
+          style={{ gap: "9px" }}
+          className="col-md-5 form-check d-flex justify-content-center"
+        >
+          <input
+            className="form-check-input"
+            type="checkbox"
+            value=""
+            id="flexCheckChecked"
+            checked
+          />
+          <label className="form-check-label" htmlFor="flexCheckChecked">
+            Remember Me
+          </label>
+        </div>
+
+        <div className="col-md-6 text-center">
+          <button
+            id="btnForgotPassword"
+            style={{
+              backgroundColor: "transparent",
+              border: "none",
+              color: "#0d6efd",
+              position: "relative",
+              left: "10px",
+            }}
+          >
+            Forgot Password ?
+          </button>
+        </div>
+      </div>
+
       <div></div>
     </aside>
   );
