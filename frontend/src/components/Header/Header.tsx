c1cfe166a4477fa34b30472d6a8e071d12c7aace
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-12 17:40:05 +0530

    Content Updated in Header Component

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -523,7 +523,6 @@ const Header = (props: HeaderProps) => {
                     {
                       <Box
                         sx={{
-                          width: "400px",
                           position: "relative",
                           backdropFilter: "static",
                         }}
