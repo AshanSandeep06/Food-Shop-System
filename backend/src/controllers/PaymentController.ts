6eacf19c18448a73867267dff746acb3204f8de8
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-14 18:41:28 +0530

    Created PaymentController with its Handler/mapping methods

---
+++ backend/src/controllers/PaymentController.ts
@@ -0,0 +1,25 @@
+import { Request, Response, RequestHandler } from 'express';
+
+const PaymentController = () => {
+    const getAllPayments: RequestHandler = async(req: Request, res: Response): Promise<Response> => {
+        return res;
+    };
+
+    const savePayment: RequestHandler = async(req: Request, res: Response): Promise<Response> => {
+        return res;
+    };
+
+    const updatePayment: RequestHandler = async(req: Request, res: Response): Promise<Response> => {
+        return res;
+    };
+
+    const deletePayment: RequestHandler = async(req: Request, res: Response): Promise<Response> => {
+        return res;
+    };
+
+    const searchPayment: RequestHandler = async(req: Request, res: Response): Promise<Response> => {
+        return res;
+    };
+};
+
+export default PaymentController;
