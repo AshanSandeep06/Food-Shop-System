745da5d89d96fa10875293f3946f6705b49d4e52
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-14 18:44:47 +0530

    Created UserController with its Handler/mapping methods

---
+++ backend/src/controllers/UserController.ts
@@ -0,0 +1,21 @@
+import { Request, Response, RequestHandler } from 'express';
+
+const UserController = () => {
+    const saveUser: RequestHandler = async(req: Request, res: Response): Promise<Response> => {
+        return res;
+    };
+
+    const updateUser: RequestHandler = async(req: Request, res: Response): Promise<Response> => {
+        return res;
+    };
+
+    const deleteUser: RequestHandler = async(req: Request, res: Response): Promise<Response> => {
+        return res;
+    };
+
+    const searchUser: RequestHandler = async(req: Request, res: Response): Promise<Response> => {
+        return res;
+    };
+};
+
+export default UserController;
