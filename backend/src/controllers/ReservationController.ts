c8061bf517a5473ad95a24deb7e63231d6a2b13d
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-15 00:47:09 +0530

    Content Updated in Controllers

--- backend/src/controllers/ReservationController.ts
+++ backend/src/controllers/ReservationController.ts
@@ -1,25 +1,23 @@
 import { Request, Response, RequestHandler } from 'express';
 
-const ReservationController = () => {
-    const getAllReservations: RequestHandler = async(req: Request, res: Response): Promise<Response> => {
+export default class ReservationController {
+    getAllReservations: RequestHandler = async(req: Request, res: Response): Promise<Response> => {
         return res;
     };
 
-    const saveReservation: RequestHandler = async(req: Request, res: Response): Promise<Response> => {
+    saveReservation: RequestHandler = async(req: Request, res: Response): Promise<Response> => {
         return res;
     };
 
-    const updateReservation: RequestHandler = async(req: Request, res: Response): Promise<Response> => {
+    updateReservation: RequestHandler = async(req: Request, res: Response): Promise<Response> => {
         return res;
     };
 
-    const deleteReservation: RequestHandler = async(req: Request, res: Response): Promise<Response> => {
+    deleteReservation: RequestHandler = async(req: Request, res: Response): Promise<Response> => {
         return res;
     };
 
-    const searchReservation: RequestHandler = async(req: Request, res: Response): Promise<Response> => {
+    searchReservation: RequestHandler = async(req: Request, res: Response): Promise<Response> => {
         return res;
     };
 };
-
-export default ReservationController;
