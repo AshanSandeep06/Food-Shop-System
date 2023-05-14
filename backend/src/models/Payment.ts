3ef63e4d1244096ca13d20c5ae06f28d72beac7c
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-14 16:05:24 +0530

    Created Payment Model in backend

--- backend/src/models/Payment.ts
+++ backend/src/models/Payment.ts
@@ -1,5 +1,44 @@
 import { Document, Schema, model } from "mongoose";
 
 export interface IPayment extends Document {
-    
+    paymentID: string;
+    orderID: string;
+    customerID: string;
+    totalAmount: number;
+    cash: number;
+    balance: number;
 };
+
+const PaymentSchema = new Schema({
+    paymentID: {
+        type: String,
+        required: true,
+    },
+
+    orderID: {
+        type: String,
+        required: true,
+    },
+
+    customerID: {
+        type: String,
+        required: true,
+    },
+
+    totalAmount: {
+        type: Number,
+        required: true,
+    },
+
+    cash: {
+        type: Number,
+        required: true,
+    },
+
+    balance: {
+        type: Number,
+        required: true,
+    },
+}, {timestamps: true});
+
+export const Payment = model<IPayment>("Payment", PaymentSchema);
