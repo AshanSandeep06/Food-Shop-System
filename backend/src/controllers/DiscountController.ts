a15eb1858e46c6b29e111f33e41dd6f7efc182eb
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-14 18:34:39 +0530

    Created DiscountController with its Handler/mapping methods

---
+++ backend/src/controllers/DiscountController.ts
@@ -0,0 +1,25 @@
+import { Request, Response, RequestHandler } from 'express';
+
+const DiscountController = () => {
+    const getAllDiscounts: RequestHandler = async(req: Request, res: Response): Promise<Response> => {
+        return res;
+    };
+
+    const saveDiscount: RequestHandler = async(req: Request, res: Response): Promise<Response> => {
+        return res;
+    };
+
+    const updateDiscount: RequestHandler = async(req: Request, res: Response): Promise<Response> => {
+        return res;
+    };
+
+    const deleteDiscount: RequestHandler = async(req: Request, res: Response): Promise<Response> => {
+        return res;
+    };
+
+    const searchDiscount: RequestHandler = async(req: Request, res: Response): Promise<Response> => {
+        return res;
+    };
+};
+
+export default DiscountController;
