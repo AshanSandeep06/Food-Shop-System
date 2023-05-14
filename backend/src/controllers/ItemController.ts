dd13dd0eb274cb341494fcc76f56f27d22a86318
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-15 00:45:45 +0530

    Content Updated in Controllers

--- backend/src/controllers/ItemController.ts
+++ backend/src/controllers/ItemController.ts
@@ -1,25 +1,23 @@
 import { Request, Response, RequestHandler } from 'express';
 
-const ItemController = () => {
-    const getAllItems: RequestHandler = async(req: Request, res: Response): Promise<Response> => {
+export default class ItemController {
+    getAllItems: RequestHandler = async(req: Request, res: Response): Promise<Response> => {
         return res;
     };
 
-    const saveItem: RequestHandler = async(req: Request, res: Response): Promise<Response> => {
+    saveItem: RequestHandler = async(req: Request, res: Response): Promise<Response> => {
         return res;
     };
 
-    const updateItem: RequestHandler = async(req: Request, res: Response): Promise<Response> => {
+    updateItem: RequestHandler = async(req: Request, res: Response): Promise<Response> => {
         return res;
     };
 
-    const deleteItem: RequestHandler = async(req: Request, res: Response): Promise<Response> => {
+    deleteItem: RequestHandler = async(req: Request, res: Response): Promise<Response> => {
         return res;
     };
 
-    const searchItem: RequestHandler = async(req: Request, res: Response): Promise<Response> => {
+    searchItem: RequestHandler = async(req: Request, res: Response): Promise<Response> => {
         return res;
     };
 };
-
-export default ItemController;
