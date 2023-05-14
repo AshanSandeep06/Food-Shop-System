a1b648ffdaaf5612c9bd3bd05d6b9626a8df8ec3
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-14 17:20:05 +0530

    Created Reservation Model in backend

---
+++ backend/src/models/Reservation.ts
@@ -0,0 +1,38 @@
+import { Document, Schema, model } from "mongoose";
+
+export interface IReservation extends Document {
+    reservationID: string;
+    customerID: string;
+    reservationDate: string;
+    reservationTime: string;
+    totalParticipants: number;
+};
+
+const ReservationSchema = new Schema({
+    reservationID: {
+        type: String,
+        required: true,
+    },
+
+    customerID: {
+        type: String,
+        required: true,
+    },
+
+    reservationDate: {
+        type: String,
+        required: true,
+    },
+
+    reservationTime: {
+        type: String,
+        required: true,
+    },
+
+    totalParticipants: {
+        type: Number,
+        required: true,
+    }
+});
+
+export const Reservation = model<IReservation>("Reservation", ReservationSchema);
