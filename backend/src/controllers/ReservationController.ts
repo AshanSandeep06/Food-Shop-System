59d4bcc52517e61b3271854c8e58740d3529593d
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-14 18:42:49 +0530

    Created ReservationController with its Handler/mapping methods

---
+++ backend/src/controllers/ReservationController.ts
@@ -0,0 +1,25 @@
+import { Request, Response, RequestHandler } from 'express';
+
+const ReservationController = () => {
+    const getAllReservations: RequestHandler = async(req: Request, res: Response): Promise<Response> => {
+        return res;
+    };
+
+    const saveReservation: RequestHandler = async(req: Request, res: Response): Promise<Response> => {
+        return res;
+    };
+
+    const updateReservation: RequestHandler = async(req: Request, res: Response): Promise<Response> => {
+        return res;
+    };
+
+    const deleteReservation: RequestHandler = async(req: Request, res: Response): Promise<Response> => {
+        return res;
+    };
+
+    const searchReservation: RequestHandler = async(req: Request, res: Response): Promise<Response> => {
+        return res;
+    };
+};
+
+export default ReservationController;
