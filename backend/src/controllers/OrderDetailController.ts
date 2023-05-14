6464e98e6db195372d651a2ce03e0099b0d964fd
AshanSandeep06 <ashansandeep06@gmail.com>
2023-05-14 18:40:04 +0530

    Created OrderDetailController with its Handler/mapping methods

---
+++ backend/src/controllers/OrderDetailController.ts
@@ -0,0 +1,17 @@
+import { Request, Response, RequestHandler } from 'express';
+
+const OrderDetailController = () => {
+    const getAllOrderDetails: RequestHandler = async(req: Request, res: Response): Promise<Response> => {
+        return res;
+    };
+
+    const saveOrderDetail: RequestHandler = async(req: Request, res: Response): Promise<Response> => {
+        return res;
+    };
+
+    const searchOrderDetail: RequestHandler = async(req: Request, res: Response): Promise<Response> => {
+        return res;
+    };
+};
+
+export default OrderDetailController;
